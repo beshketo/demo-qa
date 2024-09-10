138f14156b46d3a77cec23eee588d465419dada5
beshketo <79095417+beshketo@users.noreply.github.com>
2024-09-10 16:32:15 +0300

    Update UserFactory.java

--- web-elements/src/main/java/page/UserFactory.java
+++ web-elements/src/main/java/page/UserFactory.java
@@ -2,11 +2,7 @@ package page;
 
 public class UserFactory {
 
-
-
         public static User generateUser(String name, String email, String currentAddress, String permanentAddress) {
             return new User(name, email, currentAddress, permanentAddress);
         }
-
-
 }
