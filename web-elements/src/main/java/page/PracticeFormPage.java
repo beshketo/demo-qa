b10af53a0a40168f5d3a41661236345980c151c9
Anna Beshketo <abeshketo7@gmail.com>
2024-09-11 23:17:07 +0300

    DQA-006 Added 4 test cases from AlertPage

--- web-elements/src/main/java/page/PracticeFormPage.java
+++
@@ -1,9 +0,0 @@
-package page;
-
-import org.openqa.selenium.WebDriver;
-
-public class PracticeFormPage extends  AbstractPage{
-    public PracticeFormPage(WebDriver driver) {
-        super(driver);
-    }
-}
