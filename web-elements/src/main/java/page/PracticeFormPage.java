c3ee6254d08fc9955e7e57828be02ae1e1d2a841
Anna Beshketo <abeshketo7@gmail.com>
2024-09-11 10:23:49 +0300

    DQA-006 Added page

---
+++ web-elements/src/main/java/page/PracticeFormPage.java
@@ -0,0 +1,9 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+
+public class PracticeFormPage extends  AbstractPage{
+    public PracticeFormPage(WebDriver driver) {
+        super(driver);
+    }
+}
