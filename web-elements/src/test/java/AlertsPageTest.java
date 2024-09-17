762e31693bec03cf3143af8371e55f1a2d33e096
Anna Beshketo <abeshketo7@gmail.com>
2024-09-17 23:17:09 +0300

    DQA-007 Changed after review

--- web-elements/src/test/java/AlertsPageTest.java
+++ web-elements/src/test/java/AlertsPageTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import page.AlertsPage;
+
 import java.time.Duration;
 
 public class AlertsPageTest {
@@ -21,6 +22,7 @@ public class AlertsPageTest {
         alertsPage.openAlertsPage();
 
     }
+
     @AfterAll
     public static void tearDown() {
         if (driver != null) {
@@ -31,82 +33,68 @@ public class AlertsPageTest {
     @Test
     @Severity(SeverityLevel.NORMAL)
     @Description("checkFirstButtonAlert")
-    public void checkFirstButtonAlert(){
+    public void checkFirstButtonAlert() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("google_ads_iframe")));
         alertsPage.setClickMeButton(alertsPage.firstClickMeButton);
 
-        try {
-            wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            wait.until(ExpectedConditions.alertIsPresent());
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.alertIsPresent());
 
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            Assertions.assertEquals(alertText, "You clicked a button");
-            alert.accept();
-        } catch (Exception e){
-            System.out.println("Test failed: " + e.getMessage());
-        }
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        Assertions.assertEquals(alertText, "You clicked a button");
+        alert.accept();
     }
 
     @Test
     @Severity(SeverityLevel.NORMAL)
     @Description("checkSecondButtonAlert")
-    public void checkSecondButtonAlert(){
+    public void checkSecondButtonAlert() {
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500);");
         alertsPage.setClickMeButton(alertsPage.secondClickMeButton);
 
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
-            wait.until(ExpectedConditions.alertIsPresent());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.alertIsPresent());
 
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            Assertions.assertEquals(alertText, "This alert appeared after 5 seconds");
-            alert.accept();
-        } catch (Exception e){
-            System.out.println("Test failed: " + e.getMessage());
-        }
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        Assertions.assertEquals(alertText, "This alert appeared after 5 seconds");
+        alert.accept();
     }
+
     @Test
     @Severity(SeverityLevel.NORMAL)
     @Description("checkThirdButtonAlert")
-    public void checkThirdButtonAlert(){
+    public void checkThirdButtonAlert() {
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500);");
         alertsPage.setClickMeButton(alertsPage.thirdClickMeButton);
 
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
-            wait.until(ExpectedConditions.alertIsPresent());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.alertIsPresent());
 
-            Alert alert = driver.switchTo().alert();
-            alert.accept();
-            wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("text-success"))));
-            String confirmResult = driver.findElement(By.className("text-success")).getText();
-            String expectedResult = "You selected Ok";
-            Assertions.assertEquals(expectedResult,confirmResult,"Actual result is differ from expected result");
-        } catch (Exception e){
-            System.out.println("Test failed: " + e.getMessage());
-        }
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("text-success"))));
+        String confirmResult = driver.findElement(By.className("text-success")).getText();
+        String expectedResult = "You selected Ok";
+        Assertions.assertEquals(expectedResult, confirmResult, "Actual result is differ from expected result");
     }
 
     @Test
     @Severity(SeverityLevel.NORMAL)
     @Description("checkFourthButtonAlert")
-    public void checkFourthButtonAlert(){
+    public void checkFourthButtonAlert() {
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500);");
         alertsPage.setClickMeButton(alertsPage.fourthClickMeButton);
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
-            wait.until(ExpectedConditions.alertIsPresent());
-            Alert alert = driver.switchTo().alert();
-            alert.sendKeys("Test");
-            alert.accept();
-            String confirmResult = driver.findElement(By.className("text-success")).getText();
-            String expectedResult = "You entered Test";
-            Assertions.assertEquals(expectedResult,confirmResult,"Actual result is differ from expected result");
-        } catch (Exception e){
-            System.out.println("Test failed: " + e.getMessage());
-        }
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys("Test");
+        alert.accept();
+        String confirmResult = driver.findElement(By.className("text-success")).getText();
+        String expectedResult = "You entered Test";
+        Assertions.assertEquals(expectedResult, confirmResult, "Actual result is differ from expected result");
     }
 }
