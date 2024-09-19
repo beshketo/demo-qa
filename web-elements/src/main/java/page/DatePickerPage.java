8b9649bdd8418b5d5fa7ce25e3b0180dc0f32eb2
Anna Beshketo <abeshketo7@gmail.com>
2024-09-19 23:36:45 +0300

    DQA-008 Added 1 test case

---
+++ web-elements/src/main/java/page/DatePickerPage.java
@@ -0,0 +1,60 @@
+package page;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class DatePickerPage extends AbstractPage {
+    private static final String DATE_PICKER_PAGE_URL = baseurl + "/date-picker";
+    @FindBy(id = "datePickerMonthYearInput")
+    WebElement selectDate;
+    @FindBy(id = "dateAndTimePickerInput")
+    WebElement selectDateAndTime;
+    @FindBy(className = "react-datepicker__month")
+    WebElement datePickerCalendar;
+
+
+    public DatePickerPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Step("Open page")
+    public void openDatePickerPage() {
+        openPage(DATE_PICKER_PAGE_URL);
+    }
+
+    public String changeDataInSelectDataPicker(int number) {
+        selectDate.click();
+        List<WebElement> allDates = getAllDateElementsInCurrentMonth();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(datePickerCalendar));
+
+        if (number >= 0 && number < allDates.size()){
+            WebElement chosenDate = allDates.get(number);
+            chosenDate.click();
+            String fullDate = selectDate.getAttribute("value");
+            return fullDate;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid number provided for date selection: " + number);
+        }
+    }
+
+
+    @Step("Get all date elements in the current month")
+    public List<WebElement> getAllDateElementsInCurrentMonth() {
+        List<WebElement> allDateElementsInCurrentMonth = datePickerCalendar.findElements(By.cssSelector(".react-datepicker__day"));
+        return allDateElementsInCurrentMonth;
+    }
+
+
+}
---
+++ web-elements/src/test/java/DataPickerPageTest.java
@@ -0,0 +1,49 @@
+import config.ConfigProvider;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import page.DatePickerPage;
+import page.ModalDialogsPage;
+
+import java.time.Duration;
+
+public class DataPickerPageTest {
+    private static WebDriver driver;
+    private static DatePickerPage DatePickerPage;
+    @BeforeAll
+    public static void setUp() {
+        driver = ConfigProvider.getDriver();
+        DatePickerPage = new DatePickerPage(driver);
+        DatePickerPage.openDatePickerPage();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Description("checkChangingDateInDatePicker")
+    public void changingDateInDataPicker(){
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500);");
+
+        String expectedResult = DatePickerPage.changeDataInSelectDataPicker(5);
+        String actualResult = driver.findElement(By.id("datePickerMonthYearInput")).getAttribute("value");
+        Assertions.assertEquals(expectedResult, actualResult, "The data isn't similar");
+
+
+    };
+}
