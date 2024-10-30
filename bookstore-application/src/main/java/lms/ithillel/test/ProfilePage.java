dbc0c27530739b71631d5944c9ea3b7de00e9223
beshketo <79095417+beshketo@users.noreply.github.com>
2024-10-30 08:19:51 +0200

    Update ProfilePage.java

--- bookstore-application/src/main/java/lms/ithillel/test/ProfilePage.java
+++ bookstore-application/src/main/java/lms/ithillel/test/ProfilePage.java
@@ -1,15 +1,10 @@
 package lms.ithillel.test;
-
 import org.openqa.selenium.WebDriver;
-
 import static page.AbstractPage.baseurl;
 
 public class ProfilePage {
     public ProfilePage(WebDriver driver) {
-
         super();
-
     }
-
     final String PROFILE_PAGE_URL = baseurl + "/profile";
 }
